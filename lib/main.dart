6f52f59a52df7e4f709b3f68a6e33e226a050316
Fernanda Cristina Guerra <60983422+fercguerra@users.noreply.github.com>
2022-05-16 22:49:53 -0300

    widgets

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+// runApp é ponto de partida
+//Widget é uma classe abstrada e por isso precisa de uma isntância
+void main() => runApp(
+  Column(
+  children: const <Widget>[
+    Text('Deliver features faster'),
+    Text('Craft beautiful UIs'),
+  ],
+)
+);
+
+
+
+
